d9c64e8fd11da05609f897719d704fce8874e08d
Mayavan <149401503+Benjamindach25@users.noreply.github.com>
2024-05-17 18:42:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-      API_ID = int(os.environ.get("APP_ID", 12345))
-      API_HASH = os.environ.get("API_HASH")
-      CHANNEL = list(x for x in os.environ.get("CHANNEL_ID", "").replace("\n", " ").split(' '))
+      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6487620358:AAE4iz1l7VIALr0mUVdMp5GDhlNM5DEqQfM")
+      API_ID = int(os.environ.get("APP_ID", "27610252"))
+      API_HASH = os.environ.get("API_HASH", "73e16fc08192ba7c1d52d4dc9fa2b220")
+      CHANNEL = list(x for x in os.environ.get("CHANNEL_ID", "-1002097469844:-1002129887393").replace("\n", " ").split(' '))
